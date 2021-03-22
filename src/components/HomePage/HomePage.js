aa514c01f2a084eb5248f8618be1d469f0b94685
CO-WORKING ITMO <itmo@gmail.com>
2021-03-22 22:44:22 +0300

    I have a toothache fuck

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,29 @@
+import styles from "./styles/styles.scss"
 import React, {Component, Fragment} from 'react';
 import {Link, Redirect, Route} from "react-router-dom";
-
+import UserMenu from "./UserMenu"
 
 
 class Home extends Component {
     render() {
         return (
             <Fragment>
-                <h1>Home</h1>
+                <div className={styles.HOME}>
+                    <div className={styles.Menu}>
+                        <div className={styles.User}>
+                            <UserMenu/>
+                        </div>
+                        <div className={styles.Other}>
+                            Other
+                        </div>
+                        <div className={styles.Tasks}>
+                            Tasks
+                        </div>
+                    </div>
+                    <div className={styles.MainScreen}>
+                        MainScreen
+                    </div>
+                </div>
             </Fragment>
         )
     }
